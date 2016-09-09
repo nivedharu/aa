841f33f0656afed578284bc476e5187a9aeb5a6a
nivedharu <Nivethadharani96@gmail.com>
2016-09-09 16:52:03 +0530

    Rename d.java to d1.java

---
+++ d1.java
@@ -0,0 +1,14 @@
+package ds;
+import java.util.*;
+
+public class d
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String s="1234";
+		Integer a=Integer.parseInt(s);
+		System.out.println(a);
+	}
+}
+
