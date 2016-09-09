17931df83b215cb339731daa24b0f0e63556064a
nivedharu <Nivethadharani96@gmail.com>
2016-09-09 16:45:34 +0530

    Create d.java

---
+++ d.java
@@ -0,0 +1,23 @@
+package ds;
+import java.util.*;
+public class d {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int[] a=new int[5];
+		int i=0;
+		for(;i<a.length;i++)
+		{
+		a[i]=sc.nextInt();
+		}
+		for(int j=a.length-1;j>=0;j--)
+		{
+			System.out.print(a[j]);
+	
+
+
+	}
+
+}
+}
